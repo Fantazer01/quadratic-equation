a427ff9c503265c3b067020687645ae275bcf600
Oleg <olegkrygin9@gmail.com>
2021-10-16 00:00:51 +0300

    add library for my task

---
+++ LabTools/LabTools.cpp
@@ -0,0 +1,64 @@
+//
+// Created by oleg on 28.09.2021.
+//
+
+#include <iostream>
+
+namespace LabTools {
+
+    void skip_to_int() {
+        if (std::cin.fail()) {
+            std::cin.clear();
+            for (char ch; std::cin >> ch;) {
+                if (std::isdigit(ch) || ch == '-') {
+                    std::cin.unget();
+                    return;
+                }
+            }
+        }
+
+        throw "Ввода нет";
+    }
+
+    void getNum(int &num) {
+        while (true) {
+            if (std::cin >> num)
+                return;
+            else
+                skip_to_int();
+        }
+    }
+
+    void getNum(double &num) {
+        while (true) {
+            if (std::cin >> num)
+                return;
+            else
+                skip_to_int();
+        }
+    }
+
+    void getNum(int &num, char* const str) {
+        while (true) {
+            if (std::cin >> num)
+                return;
+            else {
+                std::cout << str << std::endl;
+                skip_to_int();
+            }
+
+        }
+    }
+
+    void getNum(double &num, char* const str) {
+        while (true) {
+            if (std::cin >> num)
+                return;
+            else {
+                std::cout << str << std::endl;
+                skip_to_int();
+            }
+
+        }
+    }
+}
---
+++ LabTools/LabTools.h
@@ -0,0 +1,17 @@
+//
+// Created by oleg on 28.09.2021.
+//
+
+#ifndef LABTOOLS_LABTOOLS_H
+#define LABTOOLS_LABTOOLS_H
+
+namespace LabTools {
+
+void getNum(int &);
+void getNum(double &);
+void getNum(int &, char* const);
+void getNum(double &, char* const);
+
+}
+
+#endif //LABTOOLS_LABTOOLS_H
